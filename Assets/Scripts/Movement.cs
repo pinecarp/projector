63ac9e0132f421b92c8668c5d58cf9cc2e611f33
pinecarp <pinecarper@icloud.com>
2022-01-18 08:43:49 +0100

    add keyboard movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+     [SerializeField] private float speed = 5;
+
+
+     
+     private void Update()
+     {
+         float horizontalinput = Input.GetAxis("Horizontal");
+         float verticalinput = Input.GetAxis("Vertical");
+         
+         transform.position += Vector3.right * horizontalinput * speed * Time.deltaTime;
+         transform.position += Vector3.up * verticalinput * speed * Time.deltaTime;
+         
+
+     }
+
+}
