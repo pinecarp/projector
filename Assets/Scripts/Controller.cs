add403b838199cf7d8858b236b9ed5fa4da5be29
pinecarp <pinecarper@icloud.com>
2022-01-19 13:55:35 +0100

    add ability to pick up items

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,32 +1,69 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-     [SerializeField] private float speed = 5;
+    private float playerSpeed = 3;
+    private float itemLerpSpeed = 7;
+    private float radius = 1;
 
+    private bool isTake;
+    
+    public Transform point;
+    
+    public GameObject pickupCanvas;
+    public GameObject item;
+    
+    public Camera mainCamera;
+    
+    public LayerMask itemsLayer;
+    
+    private Vector3 mousePos;
 
      
      private void Update()
      {
-         Movement();
+         mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
          
+         Movement();
+         Items();
+
      }
 
-
-     
-     
      private void Movement()
      {
-         
-          float horizontalinput = Input.GetAxis("Horizontal");
+         float horizontalinput = Input.GetAxis("Horizontal");
           float verticalinput = Input.GetAxis("Vertical");
          
-          transform.position += Vector3.right * horizontalinput * speed * Time.deltaTime;
-          transform.position += Vector3.up * verticalinput * speed * Time.deltaTime;
-          
+          transform.position += Vector3.right * horizontalinput * playerSpeed * Time.deltaTime;
+          transform.position += Vector3.up * verticalinput * playerSpeed * Time.deltaTime;
+     }
+     private void Items()
+     {
+         Collider2D[] itemColliders =  Physics2D.OverlapCircleAll(point.transform.position, radius, itemsLayer);
+
+         if (itemColliders.Length >= 1 & isTake == false)
+         {
+             pickupCanvas.SetActive(true);
+         }
+         else
+         {
+             pickupCanvas.SetActive(false);
+         }
+
+         if (Input.GetKeyDown(KeyCode.E) & itemColliders.Length >= 1)
+         {
+             isTake = true;
+         }
+
+         if (isTake == true)
+         {
+             item.transform.position = Vector3.Lerp(item.transform.position, new Vector3(mousePos.x,mousePos.y,5), itemLerpSpeed * Time.deltaTime);
+         }
      }
 
 }
