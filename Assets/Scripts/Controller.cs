932c95a4b8f7a666dc594fcc160730dc2737fa83
pinecarp <pinecarper@icloud.com>
2022-01-19 10:10:21 +0100

    code replacing

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+     [SerializeField] private float speed = 5;
+
+
+     
+     private void Update()
+     {
+         Movement();
+         
+     }
+
+
+     
+     
+     private void Movement()
+     {
+         
+          float horizontalinput = Input.GetAxis("Horizontal");
+          float verticalinput = Input.GetAxis("Vertical");
+         
+          transform.position += Vector3.right * horizontalinput * speed * Time.deltaTime;
+          transform.position += Vector3.up * verticalinput * speed * Time.deltaTime;
+          
+     }
+
+}
